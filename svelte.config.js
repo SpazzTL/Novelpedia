879a976f68333674a9f2395f5f3be3dc00b67981
SpazzTL <spazz.kr@gmail.com>
2025-08-05 13:57:26 -0400

    Fix paths for gh-pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,22 @@
 import adapter from '@sveltejs/adapter-static';
 
-export default {
-	kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		})
-	}
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+    kit: {
+        adapter: adapter({
+            // default options are shown. On some platforms
+            // these options are set automatically — see below for detail
+            pages: 'build',
+            assets: 'build',
+            fallback: '404.html',
+            precompress: false,
+            strict: true
+        }),
+        paths: {
+            // Set the base path to your GitHub repository name
+            base: process.argv.includes('dev') ? '' : '/Novelpedia',
+        }
+    }
 };
+
+export default config;
